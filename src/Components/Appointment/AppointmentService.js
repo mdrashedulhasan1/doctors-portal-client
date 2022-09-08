dca60d02e03cfdd9b13446c8fe15e92c068ef7ad
unknown <rashedul15-1718@diu.edu.bd>
2022-09-07 18:41:34 -0700

    V2

--- src/Components/Appointment/AppointmentService.js
+++ src/Components/Appointment/AppointmentService.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AppointmentService = ({ service }) => {
+const AppointmentService = ({ service, setTreatment }) => {
     const {name, slots} = service;
     return ( 
         <div class="card bg-base-100 shadow-xl">
@@ -9,7 +9,12 @@ const AppointmentService = ({ service }) => {
                 <p>{slots.length>0 ? <span>{slots[0]}</span>:<span className='text-red-500'>Try another date</span>}</p>
                 <p>{slots.length} {slots.length>1? 'spaces':'space'} available</p>
                 <div class="card-actions justify-center">
-                    <button disabled={slots.length===0} class="btn btn-primary">Buy Now</button>
+                <label
+                for="booking-modal" 
+                onClick={()=>setTreatment(service)}
+                disabled={slots.length===0}
+                class="btn btn-primary"
+                >Booking Appointment</label>
                 </div>
             </div>
         </div>
--- src/Components/Appointment/AvailableAppointment.js
+++ src/Components/Appointment/AvailableAppointment.js
@@ -1,9 +1,12 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import AppointmentService from './AppointmentService';
+import BookingModal from './BookingModal';
 
 const AvailableAppointment = ({ date }) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState({});
+
     useEffect(() => {
         fetch('./services.json')
             .then(res => res.json())
@@ -14,9 +17,17 @@ const AvailableAppointment = ({ date }) => {
             <h2 className='text-2xl text-primary text-center'>Available Appointments on: {format(date, 'PP')}</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <AppointmentService id={service._id} service={service}></AppointmentService>)
+                    services.map(service => <AppointmentService
+                        id={service._id}
+                        setTreatment={setTreatment}
+                        service={service}
+                    ></AppointmentService>)
                 }
+
             </div>
+                {
+                    treatment && <BookingModal treatment={treatment}></BookingModal>
+                }
         </div>
 
     );
