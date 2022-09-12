86a67b5036f169e764b86336763234cee254f1e7
unknown <rashedul15-1718@diu.edu.bd>
2022-09-12 09:24:27 -0700

    Add Login Page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if(user){
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+    if (user) {
         console.log(user);
     }
     return (
@@ -11,6 +16,55 @@ const Login = () => {
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Email Address"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
+                                        message: 'Provide a valid Email'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className='text-red-500'>{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className='text-red-500'>{errors.email.message}</span>}
+                            </label>
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="password"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be 6 character on longer a valid email' // JS only: <p>error message</p> TS only support string
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className='text-red-500'>{errors.password.message}</span>}
+                                {errors.pasword?.type === 'minLength' && <span className='text-red-500'>{errors.password.message}</span>}
+                            </label>
+                        </div>
+                        <input type="submit" className="btn btn-secondary text-white w-full" value="Login" />
+                    </form>
                     <div className="divider">OR</div>
                     <button onClick={() => signInWithGoogle()} className="btn btn-outline btn-primary">signIn_With_Google</button>
                 </div>
